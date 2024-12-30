e8d79adacffc1bff0a8723336bdd314f44df6249
Sushma Shrestha <sushmashrestha@mercantilecloud.com.np>
2024-12-30 16:24:11 +0545

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules
+: [
+    '@nuxt/test-utils/module'
+  ],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
