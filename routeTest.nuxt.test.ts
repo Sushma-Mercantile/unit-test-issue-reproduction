fc64350160c87e35f29de0c68c3dcfaee2a1bacf
Sushma Shrestha <sushmashrestha@mercantilecloud.com.np>
2025-01-02 12:00:51 +0545

    adding failing and passing test

--- routeTest.nuxt.test.ts
+++ routeTest.nuxt.test.ts
@@ -2,12 +2,19 @@ import { setup, $fetch, createPage, url } from '@nuxt/test-utils/e2e'
 import { describe, test, expect } from 'vitest'
 
 describe('test', async () => {
-    await setup()
+    await setup();
 
-    test('contains number as string', async () => {
+    test('contains hellot as string', async () => {
         console.log("test");
-        const html = await $fetch('/')
+        const html = await $fetch('/');
         console.log(html);
-        // expect(html).toContain('Number:')
+        expect(html).toContain('Hellot');
+    })
+
+    test('contains hello as string', async () => {
+        console.log("test");
+        const html = await $fetch('/');
+        console.log(html);
+        expect(html).toContain('Hello');
     })
 })
